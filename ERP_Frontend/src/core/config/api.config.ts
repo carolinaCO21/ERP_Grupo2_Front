8a1a3d8ecb7820aae174dba52fbb03b2d729b986
inesgarcia-nervion <ines.garcia@iesnervion.es>
2026-02-07 19:21:51 +0100

    Cambio en el core

--- ERP_Frontend/src/core/config/api.config.ts
+++ ERP_Frontend/src/core/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  baseUrl: 'https://tu-api.azurewebsites.net/api',
+  baseUrl: 'https://tu-api.azurewebsites.net/api',          // Cambiar URL 
   endpoints: {
     pedidos: '/pedidos',
     proveedores: '/proveedores',
